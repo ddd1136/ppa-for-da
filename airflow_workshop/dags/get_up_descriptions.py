a4eda9862aa1542c362ceac72b46049dc2c57b98
ddd1136 <123336438+ddd1136@users.noreply.github.com>
2024-10-30 16:26:14 +0200

    Update get_up_descriptions.py

--- airflow_workshop/dags/get_up_descriptions.py
+++ airflow_workshop/dags/get_up_descriptions.py
@@ -1,57 +1,76 @@
-import requests
+import os
 import pandas as pd
 import pendulum
-import json
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.python import PythonOperator
-from airflow.models import Variable
+from airflow.settings import json
 
-url_auth = "https://id.itmo.ru/auth/realms/itmo/protocol/openid-connect/token"
-client_id = Variable.get ("client_id")
-client_secret = Variable.get ("client_secret")
-grant_type = Variable.get ("grant_type")
-auth_data = {"client_id": client_id, "client_secret": client_secret, "grant_type": grant_type}
+def parse_file(file_path):
+    required_fields = [
+        "plan_type",
+        "direction_id",
+        "ns_id",
+        "direction_code",
+        "direction_name",
+        "edu_program_id",
+        "edu_program_name",
+        "faculty_id",
+        "faculty_name",
+        "training_period",
+        "university_partner",
+        "up_country",
+        "lang",
+        "military_department",
+        "total_intensity",
+        "ognp_id",
+        "ognp_name",
+        "selection_year",
+    ]
 
-def get_up(up_id):
-    up_id = str(up_id)
-    url = 'https://disc.itmo.su/api/v1/academic_plans/' + up_id
-    page = requests.get(url, headers=headers)
-    df = pd.DataFrame(page.json()['result'])
+    with open(file_path, 'r') as f:
+        json_data = {**json.load(f), 'disciplines_blocks': None}
+
+    print("[UP DESCRIPTION] Parse file:", file_path)
+
+    is_all_required_fields = all([
+        field in list(json_data.keys())
+        for field in required_fields
+    ])
+
+    if not is_all_required_fields:
+        print("[UP DESCRIPTION] Error: skipping file, not all required fields", file_path)
+        return pd.DataFrame()
+
+    df = pd.DataFrame([json_data])
     df = df.drop(['disciplines_blocks'], axis=1)
-    if len(df)>0:
-        PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.up_description', df.values, target_fields=df.columns.tolist(), replace=True, replace_index='id')
+
+    return df
+
 
 def get_up_description():
-    ids = PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').get_records(
-    """
-    select (json_array_elements(academic_plan_in_field_of_study::json)->>'ap_isu_id')::integer as ap_isu_id from stg.work_programs wp 
-    order by 1
-    """)
-    start = 0
-    finish = start + 100
-    while start < len(ids):
-        token_txt = requests.post(url_auth, auth_data).text
-        token = json.loads(token_txt)["access_token"]
-        headers = {'Content-Type': "application/json", 'Authorization': "Token " + token}
-        if finish > len(ids): finish = len(ids) + 1
-        for up_id in ids[start:finish]:
-            up_id = str(up_id[0])
-            url = 'https://disc.itmo.su/api/v1/academic_plans/' + up_id
-            print (url)
-            page = requests.get(url, headers=headers)
-            print (page)
-            df = pd.DataFrame(page.json()['result'])
-            df = df.drop(['disciplines_blocks'], axis=1)
-            if len(df)>0:
-                PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows('stg.up_description', df.values, target_fields=df.columns.tolist(), replace=True, replace_index='id')
-        start += 100
-        finish = start + 100
+    base_path = '/opt/airflow/data'  # Путь, где находятся JSON-файлы внутри контейнера
 
-with DAG(dag_id='get_up_descriptions', start_date=pendulum.datetime(2022, 1, 1, tz="UTC"), schedule_interval='0 5 * * 0', catchup=False) as dag:
+    for root, _, files in os.walk(base_path):
+        for file_name in files:
+            if file_name.endswith('.json'):
+                file_path = os.path.join(root, file_name)
+                print(file_path)
+                df = parse_file(file_path)
+
+                if df.empty:
+                    continue
+
+                PostgresHook(postgres_conn_id='PG_WAREHOUSE_CONNECTION').insert_rows(
+                    'stg.up_description', df.values, target_fields=df.columns.tolist(), replace=True,
+                    replace_index='id')
+
+
+with DAG(dag_id='get_up_descriptions', start_date=pendulum.datetime(2024, 1, 1, tz="UTC"), schedule_interval="@monthly",
+         catchup=False) as dag:
     t1 = PythonOperator(
-    task_id='get_up_description',
-    python_callable=get_up_description
+        task_id='get_up_descriptions',
+        python_callable=get_up_description
     )
 
 t1 
