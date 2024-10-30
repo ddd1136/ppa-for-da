2a8ccf036683546c128b89931a41c6994eb9f379
ddd1136 <123336438+ddd1136@users.noreply.github.com>
2024-10-30 16:24:08 +0200

    Update dds_ddl.sql

--- airflow_workshop/sql/dds_ddl.sql
+++ airflow_workshop/sql/dds_ddl.sql
@@ -31,116 +31,72 @@
 --(id integer, title text, institution varchar (100), discipline_code integer);
 
 
--- Таблица dds.states
-CREATE TABLE dds.states
-(
-    id serial PRIMARY KEY,  -- id как PRIMARY KEY
-    cop_state varchar(2),
-    state_name varchar(20)
-);
+create table dds.states
+(id serial, cop_state varchar(2), state_name varchar(20));
 ALTER TABLE dds.states ADD CONSTRAINT state_name_uindex UNIQUE (state_name);
-
--- Таблица dds.directions
-CREATE TABLE dds.directions
-(
-    id serial PRIMARY KEY,  -- id как PRIMARY KEY
-    direction_id integer,
-    direction_code varchar(10),
-    direction_name varchar(100)
-);
-ALTER TABLE dds.directions ADD CONSTRAINT direction_code_uindex UNIQUE (direction_code);
-
--- Таблица dds.levels
-CREATE TABLE dds.levels
-(
-    id serial PRIMARY KEY,  -- id как PRIMARY KEY
-    training_period varchar(5),
-    level_name varchar(20)
-);
-ALTER TABLE dds.levels ADD CONSTRAINT level_name_uindex UNIQUE (level_name);
-
--- Таблица dds.editors
-CREATE TABLE dds.editors
-(
-    id integer PRIMARY KEY,  -- id как PRIMARY KEY
-    username varchar(50),
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(50),
-    isu_number varchar(6)
-);
+create table dds.editors
+(id integer, username varchar(50), first_name varchar(50), last_name varchar(50), email varchar(50), isu_number varchar(6));
 ALTER TABLE dds.editors ADD CONSTRAINT editors_uindex UNIQUE (id);
-
--- Таблица dds.units
-CREATE TABLE dds.units
-(
-    id integer PRIMARY KEY,  -- id как PRIMARY KEY
-    unit_title varchar(100),
-    faculty_id integer
-);
+create table dds.up
+(app_isu_id integer, on_check varchar(20), laboriousness integer, year integer, qualification varchar(20), update_ts timestamp, faculty varchar(50));
+create table dds.wp
+(wp_id integer, discipline_code integer, wp_title text, wp_status integer, unit_id integer, wp_description text, update_ts timestamp);
+ALTER TABLE dds.wp
+ALTER COLUMN discipline_code TYPE text;
+create table dds.wp_editor
+(wp_id integer, editor_id integer, update_ts timestamp);
+create table dds.units
+(id integer, unit_title varchar(100), faculty_id integer);
 ALTER TABLE dds.units ADD CONSTRAINT units_uindex UNIQUE (id);
 
--- Таблица dds.up
-CREATE TABLE dds.up
-(
-    id integer PRIMARY KEY,  -- id как PRIMARY KEY
-    plan_type varchar(8),
-    direction_id integer REFERENCES dds.directions(id),  -- FOREIGN KEY к directions
-    ns_id integer,
-    edu_program_id integer,
-    edu_program_name text,
-    unit_id integer REFERENCES dds.units(id),  -- FOREIGN KEY к units
-    level_id integer REFERENCES dds.levels(id),  -- FOREIGN KEY к levels
-    university_partner text,
-    up_country text,
-    lang text,
-    military_department boolean,
-    selection_year integer
-);
+-- Добавляем первичный ключ для id в таблицу states
+ALTER TABLE dds.states
+ADD CONSTRAINT states_pk PRIMARY KEY (id);
+ADD CONSTRAINT state_name_uindex UNIQUE (state_name);
 
--- Таблица dds.wp
-CREATE TABLE dds.wp
-(
-    wp_id integer PRIMARY KEY,  -- wp_id как PRIMARY KEY
-    discipline_code text,
-    wp_title text,
-    wp_status integer,
-    unit_id integer REFERENCES dds.units(id),  -- FOREIGN KEY к units
-    wp_description text
-);
+-- Добавляем первичный ключ для id в таблицу editors
+ALTER TABLE dds.editors
+ADD CONSTRAINT editors_pk PRIMARY KEY (id);
+ADD CONSTRAINT editors_uindex UNIQUE (editor_id);
 
--- Таблица dds.wp_editor
-CREATE TABLE dds.wp_editor
-(
-    wp_id integer REFERENCES dds.wp(wp_id),  -- FOREIGN KEY к wp
-    editor_id integer REFERENCES dds.editors(id)  -- FOREIGN KEY к editors
-);
+-- Добавляем первичный ключ для app_isu_id в таблицу up
+ALTER TABLE dds.up
+ADD CONSTRAINT up_pk PRIMARY KEY (app_isu_id);
 
--- Таблица dds.wp_up
-CREATE TABLE dds.wp_up
-(
-    wp_id integer REFERENCES dds.wp(wp_id),  -- FOREIGN KEY к wp
-    up_id integer REFERENCES dds.up(id)  -- FOREIGN KEY к up
-);
+-- Добавляем первичный ключ для wp_id в таблицу wp
+ALTER TABLE dds.wp
+ADD CONSTRAINT wp_pk PRIMARY KEY (wp_id);
 
--- Таблица dds.wp_markup
-CREATE TABLE dds.wp_markup
-(
-    id integer PRIMARY KEY,  -- id как PRIMARY KEY
-    title text,
-    discipline_code integer,
-    prerequisites text,
-    outcomes text,
-    prerequisites_cnt smallint,
-    outcomes_cnt smallint
-);
-ALTER TABLE dds.wp_markup ADD CONSTRAINT wp_id_uindex UNIQUE (id);
+-- Добавляем составной первичный ключ для wp_id и editor_id в таблицу wp_editor
+ALTER TABLE dds.wp_editor
+ADD CONSTRAINT wp_editor_pk PRIMARY KEY (wp_id, editor_id);
 
--- Таблица dds.online_courses
-CREATE TABLE dds.online_courses
-(
-    id integer PRIMARY KEY,  -- id как PRIMARY KEY
-    title text,
-    institution varchar(100),
-    discipline_code integer
-);
+-- Добавляем первичный ключ для id в таблицу units
+ALTER TABLE dds.units
+ADD CONSTRAINT units_pk PRIMARY KEY (id);
+
+
+-- Добавляем внешний ключ для wp_status в таблицу states
+ALTER TABLE dds.wp
+ADD CONSTRAINT fk_wp_status
+FOREIGN KEY (wp_status) REFERENCES dds.states(id);
+
+-- Добавляем внешний ключ для unit_id в таблицу units
+ALTER TABLE dds.wp
+ADD CONSTRAINT fk_wp_unit_id
+FOREIGN KEY (unit_id) REFERENCES dds.units(id);
+
+-- Добавляем внешний ключ для editor_id и wp_id в таблице wp_editor
+ALTER TABLE dds.wp_editor
+ADD CONSTRAINT fk_wp_editor_editor
+FOREIGN KEY (editor_id) REFERENCES dds.editors(id);
+ALTER TABLE dds.wp_editor
+ADD CONSTRAINT fk_wp_editor_wp_update
+FOREIGN KEY (wp_id, update_ts) REFERENCES dds.wp (wp_id, update_ts);
+
+-- Добавляем внешний ключ для wp_unit в таблицу wp
+ALTER TABLE dds.wp
+ADD CONSTRAINT fk_wp_unit
+FOREIGN KEY (unit_id) REFERENCES dds.units(id);
+
+alter table dds.up add column unit text
